f5f06bceceec0fe2adc4676a4db9c3c82706c072
Seth8277 <mail@imseth.cn>
2018-03-31 22:24:28 +0800

    格式化代码，优化评论框

--- layouts/duoshuo.php
+++ layouts/duoshuo.php
@@ -1,20 +1,20 @@
-<?php 
+<?php
 
-	/**
-	 * DISQUS COMMENTS
-	 */
+/**
+ * DISQUS COMMENTS
+ */
 
 ?>
 
-<?php if (!is_home()&&(comments_open()) ){ ?>
-	<section class="duoshuowrapper comments">
-		<div class="commentwrap comments-hidden">
-			<div class="notification"><i class="iconfont">&#xe667;</i><?php esc_html_e('查看评论', 'akina'); ?></div>
-		</div>
-		<div class="comments-main">
-			<div class="commentwrap">
-				<?php comments_template('', true); ?>
-			</div>
-		</div>
-	</section>
-	<?php } ?>
+<?php if (!is_home() && (comments_open())) { ?>
+    <section class="duoshuowrapper comments">
+        <div class="commentwrap comments-hidden">
+            <div class="notification"><i class="iconfont">&#xe667;</i><?php esc_html_e('查看评论', 'akina'); ?></div>
+        </div>
+        <div class="comments-main">
+            <div class="commentwrap">
+                <?php comments_template('', true); ?>
+            </div>
+        </div>
+    </section>
+<?php } ?>
--- single.php
+++ single.php
@@ -8,17 +8,17 @@
  */
 
 get_header(); ?>
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-		<?php
-		while ( have_posts() ) : the_post();
-			get_template_part( 'tpl/content', 'single' );
-			get_template_part('layouts/post','nextprev');  
-		    get_template_part('layouts/authorprofile'); 
-		endwhile; // End of the loop.
-		?>
-		</main><!-- #main -->
-	</div><!-- #primary -->
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+            <?php
+            while (have_posts()) : the_post();
+                get_template_part('tpl/content', 'single');
+                get_template_part('layouts/post', 'nextprev');
+                get_template_part('layouts/authorprofile');
+            endwhile; // End of the loop.
+            ?>
+        </main><!-- #main -->
+    </div><!-- #primary -->
 <?php
 get_sidebar();
 get_footer();
